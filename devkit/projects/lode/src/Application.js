beee9143c9143c2bfa927282f4cb17925aa9d9ec
Danielle Levi <dslevi12@gmail.com>
2013-07-21 18:53:06 -0700

    Set up parallax view and add sprite to game layer

--- devkit/projects/lode/src/Application.js
+++ devkit/projects/lode/src/Application.js
@@ -1,15 +1,50 @@
 import ui.TextView as TextView;
+import ui.View;
+import ui.ImageView;
+import ui.SpriteView;
+
+import src.platformer.ParallaxView as ParallaxView;
+import src.platformer.GestureView as GestureView;
+import src.platformer.Physics as Physics;
+import src.platformer.ScoreView as ScoreView;
+import src.platformer.util as util;
 
 exports = Class(GC.Application, function () {
 
 	this.initUI = function () {
-		var textview = new TextView({
+		util.scaleRootView(this, 1024, 576);
+		this.setupParallaxView();
+		this.setupPlayer();
+		this.gameLayer.addSubview(this.player);
+	};
+	
+	this.setupParallaxView = function () {
+		this.parallaxView = new ParallaxView({
 			superview: this.view,
-			layout: "box",
-			text: "Hello, world!",
-			color: "white"
+			width: this.view.style.width,
+			height: this.view.style.height,
+		});
+		this.gameLayer = this.parallaxView.addLayer({
+			distance: 7,
+			populate: function (layer, x) {
+				return null;
+			}.bind(this)
 		});
 	};
 	
-	this.launchUI = function () {};
+	this.setupPlayer = function () {
+		this.player = new ui.SpriteView({
+			zIndex: 1,
+			x: 0,
+			y: 0,
+			anchorX: 50,
+			anchorY: 50,
+			autoSize: true,
+			url: 'resources/images/avatarKiwiAce/kiwiAce',
+			defaultAnimation: 'focused',
+			autoStart: true,
+		});
+	};
+	
+	
 });
